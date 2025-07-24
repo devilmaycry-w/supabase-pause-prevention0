5077148ab5689be60530de8f8cb1cabe5ab72e0a
Ankrit Maity <realankrit@gmail.com>
2025-07-24 18:49:04 +0530

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,13 +1,7 @@
-/**
- * Same thing as /utils/supabase/component.ts for pages/-type app
- */
+// utils/supabase/client.ts
+import { createClient } from '@supabase/supabase-js'
 
-
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
